88ec50a589649d692011cf70af346ec75b04e4b1
Ritin Sundriyal <173669837+Nitir4@users.noreply.github.com>
2025-04-22 16:08:40 +0530

    Create Libraries.py

---
+++ ENTERk/Libraries.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2
+from tkinter import *
+from tkinter import filedialog, messagebox
+from tkinterdnd2 import TkinterDnD, DND_FILES
+from PIL import Image, ImageTk
+import os
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
